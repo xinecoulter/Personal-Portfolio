7dd9ac97c4063b9a783b9ba75ba3859a97f77a8d
Christine Coulter <christinetcoulter@gmail.com>
2013-08-25 19:55:35 -0400

    New Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalPortfolio::Application.config.secret_token = 'aa342ee3927867850169f850ad8f82ff5275e6c57ded76f16c47923192aa4984a64903bbfca7dabde92df94ce22e353fb4a1bb9b9378754e1fc26d9bbc7537d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalPortfolio::Application.config.session_store :cookie_store, key: '_personal_portfolio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PersonalPortfolio::Application.config.session_store :active_record_store
