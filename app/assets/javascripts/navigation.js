cf010a9d14d35d2236bed10e39bc90fbb90b79fc
Christine Coulter <christinetcoulter@gmail.com>
2013-08-29 17:21:23 -0400

    Add to bio in welcome index view. Create navigation.js for fading transitions between routes.

---
+++ app/assets/javascripts/navigation.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  $("#bio").click(function(event){
+      event.preventDefault();
+      newLocation = "/projects";
+      $('body').fadeOut(500, newpage);
+  });
+
+
+
+  $('body').css('display', 'none');
+
+  $('body').fadeIn(1000);
+
+
+  $('a').click(function(event) {
+    event.preventDefault();
+    newLocation = this.href;
+    $('body').fadeOut(500, newpage);
+  });
+
+
+  function newpage() {
+    window.location = newLocation;
+  }
+
+});
