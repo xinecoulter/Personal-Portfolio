1ae0ca381ee3019d63d37a771c796d8845d1630e
Christine Coulter <christinetcoulter@gmail.com>
2013-08-29 21:44:58 -0400

    Add class to certain navbar links so that other links can still open in new tab. Edit navigation.js file to apply fade transitions to links of that class.

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $('body').css('display', 'none');
     $('body').fadeIn(1000);
 
-    $('a').click(function(event) {
+    $('.portfolio-link').click(function(event) {
         event.preventDefault();
         newLocation = this.href;
         $('body').fadeOut(500, newpage);
