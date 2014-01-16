c37b705d1828fde9ea8b5597e7affaf639199adf
Christine Coulter <christinetcoulter@gmail.com>
2014-01-16 00:01:36 -0500

    Add line to application.rb file to prevent precompile. Attempt to address issue with pushing to Heroku.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+config.assets.initialize_on_precompile = false
+
 module PersonalPortfolio
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
